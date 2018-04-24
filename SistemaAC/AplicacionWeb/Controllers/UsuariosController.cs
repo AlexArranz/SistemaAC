8e4ef194baaff78671cfcf9f0a11b2cf7c3b6240
Unknown <alexrnz.46@gmail.com>
2018-04-24 11:14:07 +0200

    Vista usuario modificada
    Implementación para mostrar el rol de cada usuario en la lista de usuario

--- SistemaAC/AplicacionWeb/Controllers/UsuariosController.cs
+++ SistemaAC/AplicacionWeb/Controllers/UsuariosController.cs
@@ -7,22 +7,60 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using AplicacionWeb.Data;
 using AplicacionWeb.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace SistemaAC.Controllers
 {
     public class UsuariosController : Controller
     {
         private readonly ApplicationDbContext _context;
+        UserManager<ApplicationUser> _userManager;
+        RoleManager<IdentityRole> _roleManager;
+        UsuarioRole _usuarioRole;
+        public List<SelectListItem> usuarioRole;
 
-        public UsuariosController(ApplicationDbContext context)
+        public UsuariosController(ApplicationDbContext context, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
         {
             _context = context;
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _usuarioRole = new UsuarioRole();
+            usuarioRole = new List<SelectListItem>();
         }
 
         // GET: Usuarios
+        /// <summary>
+        /// Método para obtener un listado de todos los usuarios
+        /// </summary>
+        /// <returns>Retorna una vista</returns>
         public async Task<IActionResult> Index()
         {
-            return View(await _context.Users.ToListAsync());
+            var ID = "";
+            //Objeto list que depende de la clase Usuario
+            List<Usuario> usuario = new List<Usuario>();
+            /* Obtengo todos los registros de la tabla donde almaceno
+             * los usuarios y lo almaceno en el objeto
+             */
+            var appUsuario = await _context.Users.ToListAsync();
+            /* Con una estructura de control iterativa foreach recorremos
+             * todos los valores del objeto appUsuario
+             */
+             foreach ( var Data in appUsuario)
+            {
+                ID = Data.Id;
+                usuarioRole = await _usuarioRole.GetRole(_userManager, _roleManager, ID);
+
+                usuario.Add(new Usuario()
+                {
+                    Id = Data.Id,
+                    UserName = Data.UserName,
+                    PhoneNumber = Data.PhoneNumber,
+                    Email = Data.Email,
+                    Role = usuarioRole[0].Text
+                });
+            }
+            //return View(await _context.Users.ToListAsync());
+            return View(usuario.ToList());
         }
 
         /// <summary>
